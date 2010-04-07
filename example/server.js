a57e3fe99da9a011dc44c77565800c24459db475
rick <technoweenie@gmail.com>
2010-04-06 22:03:21 -0700

    add a working example

--- example/server.js
+++ example/server.js
@@ -1,5 +1,28 @@
-var dealer = require("./lib/index"),
-       sys = require('sys')
+var redisClient = require("redisclient"),
+         dealer = require("../lib/index"),
+            sys = require('sys')
 
 conn = dealer.create()
 conn.server.listen(3840)
+
+conn.addListener('connect', function(client) {
+  sys.puts('connected to ' + client.channel + ': '+ client.id)
+})
+
+conn.addListener('disconnect', function(client) {
+  sys.puts('disconnected: ' + client.id)
+})
+
+conn.addListener('receive', function(client, data) {
+  sys.puts('from ' + client.id + '(' + client.channel + '): ' + data)
+  conn.send(client, "PONG! " + data)
+})
+
+var redis = redisClient.createClient();
+    
+redis.stream.addListener("connect", function () {
+  redis.subscribeTo("*", function (channel, message) {
+    sys.puts("to " + channel + ": " + message);
+    conn.sendToChannel(channel, message)
+  })
+})
--- server.js
+++
@@ -1,5 +0,0 @@
-var dealer = require("./lib/index"),
-       sys = require('sys')
-
-conn = dealer.create()
-conn.server.listen(3840)
