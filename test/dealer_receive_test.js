75875c357297ed8aae94ae0b9de3f8fb9f17e7d2
rick <technoweenie@gmail.com>
2010-04-06 20:53:43 -0700

    test dealer receive method

---
+++ test/dealer_receive_test.js
@@ -0,0 +1,22 @@
+var dealer = require('../lib'),
+    assert = require('assert'),
+       sys = require('sys')
+
+var d = dealer.create()
+
+expectedEvents = 1
+expectedData   = 'hello'
+expectedClient = {id: 'abc', socket: 'a', channel: 'foo'}
+
+d.addListener('receive', function(client, data) {
+  assert.ok(expectedEvents > 0)
+  expectedEvents = expectedEvents - 1
+  assert.deepEqual(expectedClient, client)
+  assert.equal(expectedData,       data)
+})
+
+var client = d.connect('a', '/foo?id=abc')
+d.receive(client, 'hello')
+
+assert.equal(0, expectedEvents)
+sys.puts('pass!')
